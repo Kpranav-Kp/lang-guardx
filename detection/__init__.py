7aded2024e0bee02e3c876e57d14cf9f3a073a92
Kpranav-Kp <pranav2005.kp@gmail.com>
2026-02-22 21:53:59 +0530

    Module 1 : Working v1

--- detection/__init__.py
+++ detection/__init__.py
@@ -1,1 +1,17 @@
 # All available detection functions are imported here for easy access
+from .core import Detector, DetectionResult
+from .bloom import BloomDetector
+from .regex import RegexDetector, RegexMatch
+from .sql_intent import SQLIntentClassifier
+from .indirect import IndirectScanner, ScanResult
+
+__all__ = [
+    "Detector",
+    "DetectionResult",
+    "BloomDetector",
+    "RegexDetector",
+    "RegexMatch",
+    "SQLIntentClassifier",
+    "IndirectScanner",
+    "ScanResult",
+]
